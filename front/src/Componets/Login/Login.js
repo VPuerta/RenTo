b1499d6d8f119bb902ede5a87df3b92c3cf50d12
Vanesa Puerta <Vanesa@MacBook-Air-de-Vanesa.local>
2019-07-26 17:16:32 +0200

    update login

--- front/src/Componets/Login/Login.js
+++ front/src/Componets/Login/Login.js
@@ -1,58 +1,59 @@
 import React, { Component } from 'react';
 import AuthServices from '../../Services/Services'
-import { Link, Redirect } from 'react-router-dom';
-
+import './Login.css'
+import { Link } from 'react-router-dom';
+import logo from './Assets/logo-white.png';
 
 class Login extends Component {
-  constructor(props){
-    super(props);
-    this.state = { username: '', password: '' };
-    this.service = new AuthServices();
-  }
+    constructor(props){
+        super(props);
+        this.state = { username: '', password: '' };
+        this.service = new AuthServices();
+    }
 
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    const username = this.state.username;
-    const password = this.state.password;
-    this.service.login(username, password)
-    .then( response => {
-        this.setState({ username: "", password: "" });
-        this.props.getUser(response)
-    })
-    .catch( error => console.log(error) )
-  }
-    
-  handleChange = (event) => {  
-    const {name, value} = event.target;
-    this.setState({[name]: value});
-  }
-    
-  render(){
-    
-    return(
-      <div className="form">
-        <form onSubmit={this.handleFormSubmit}>
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+        const username = this.state.username;
+        const password = this.state.password;
+        this.service.login(username, password)
+            .then( response => {
+                this.setState({ username: "", password: "" });
+                this.props.getUser(response)
+            })
+            .catch(error => console.log(error))
+    };
 
-        <div className="form-group">
-          <label>Username:</label>
-          <input type="text" name="username" placeholder="Name" value={this.state.username} onChange={ e => this.handleChange(e)}/>
-          </div>
+    handleChange = (event) => {
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+    };
 
-          <div className="form-group">
-          <label>Password:</label>
-          <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={ e => this.handleChange(e)} />
-          </div>
-
-          <input  className="btn btn-primary btn-lg" type="submit" value="Login" />
-        </form>
-
-        <p>Don't have account? 
-            <Link to={"/signup"}> Signup</Link>
-        </p>
-      </div>
-      
-    )
-    
-  }
+    render(){
+        return(
+            <div>
+                <div className="login-clean">
+                    <form onSubmit={this.handleFormSubmit}>
+                        <h2 className="sr-only">Login Form</h2>
+                        <div className="illustration">
+                            <img className="rounded-circle" src={logo} width={150} alt="logo"/>
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" name="username" placeholder="Username" value={this.state.username} onChange={ e => this.handleChange(e)}/>
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" type="password" name="password" placeholder="Password" value={this.state.password} onChange={ e => this.handleChange(e)}/>
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-primary btn-block" type="submit" value="Login" >Log In</button>
+                        </div>
+                        <p className="forgot">
+                            Don't have account? <Link to={"/signup"}> Signup</Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        )
+    }
 }
+
 export default Login;
--- front/src/Componets/Signup/Signup.js
+++ front/src/Componets/Signup/Signup.js
@@ -2,79 +2,77 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import AuthServices from '../../Services/Services'
 import './Signup.css'
+import logo from "../Login/Assets/logo-white.png";
 
 export default class Signup extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = { 
-      username: '', 
-      password: '',
-      city:'',
-      email: '',
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            city:'',
+            email: '',
+        };
+        this.service = new AuthServices();
+    }
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+        const username = this.state.username;
+        const password = this.state.password;
+        const city = this.state.city
+        const email = this.state.email;
+
+        this.service.signup(username, password,city,email)
+            .then( response => {
+                this.setState({
+                    username: "",
+                    password: "",
+                    city:"",
+                    email: "",
+                });
+                this.props.getUser(response)
+            })
+            .catch(error => console.log(error) )
     };
-    this.service = new AuthServices();
-  }
+
+    handleChange = (event) => {
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+    };
 
 
-  handleFormSubmit = (event) => {
-  event.preventDefault();
-  const username = this.state.username;
-  const password = this.state.password;
-  const city = this.state.city
-  const email = this.state.email;
-
-  this.service.signup(username, password,city,email)
-  .then( response => {
-      this.setState({
-          username: "", 
-          password: "",
-          city:"",
-          email: "",
-      });
-      this.props.getUser(response)
-  })
-  .catch( error => console.log(error) )
-}
-
-handleChange = (event) => {  
-  const {name, value} = event.target;
-  this.setState({[name]: value});
-}
-    
-
-render(){
-  return(
-    <div className="form">
-      <form onSubmit={this.handleFormSubmit}>
-      <div className="form-group">
-        <label>Username:</label>
-        <input type="text" name="username" placeholder="Name" value={this.state.username} onChange={ e => this.handleChange(e)}/>
-        </div>
-
-        <div className="form-group">
-        <label>Password:</label>
-        <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={ e => this.handleChange(e)} />
-        </div>
-
-        <div className="form-group">
-        <label>City:</label>
-        <input type="text" name="city" placeholder="City" value={this.state.city} onChange={ e => this.handleChange(e)}/>
-        </div>
-
-        <div className="form-group">
-        <label>Email:</label>
-        <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={ e => this.handleChange(e)}/>
-        </div>
-
-        <input className="btn btn-primary btn-lg" type="submit" value="Signup" />
-      </form>
-
-      <p>Already have account? 
-          <Link to={"/login"}> Login</Link>
-      </p>
-
-    </div>
-  )
-}
+    render(){
+        return(
+            <div>
+                <div className="login-clean">
+                    <form onSubmit={this.handleFormSubmit}>
+                        <h2 className="sr-only">Login Form</h2>
+                        <div className="illustration">
+                            <img className="rounded-circle" src={logo} width={150} alt="logo"/>
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" type="text" name="username" placeholder="Username" value={this.state.username} onChange={ e => this.handleChange(e)}/>
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" type="password" name="password" placeholder="Password" value={this.state.password} onChange={ e => this.handleChange(e)}/>
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" type="text" name="city" placeholder="City" value={this.state.city} onChange={ e => this.handleChange(e)}/>
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" type="email" name="email" placeholder="Email" value={this.state.email} onChange={ e => this.handleChange(e)}/>
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-primary btn-block" type="submit" value="Signup" >Signup</button>
+                        </div>
+                        <p className="forgot">
+                            Already have an account? <Link to={"/login"}>Log in</Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        )
+    }
 }
